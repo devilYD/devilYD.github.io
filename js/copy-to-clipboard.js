9bf1bba8aad1d8e8e8cefcfb6cc1fa473f6369aa
devilYD <devilyudun@gmail.com>
2026-03-08 15:26:30 +0800

    为代码块添加复制功能

--- js/copy-to-clipboard.js
+++ js/copy-to-clipboard.js
@@ -1,3 +1,6 @@
+// copy-to-clipboard.js
+// 使用 ClipboardJS 实现代码块复制功能
+// 页面加载完成后遍历所有 code 元素，为每个 code 块添加一个复制按钮，并绑定复制事件
 document.addEventListener('DOMContentLoaded', function () {
     var codeBlocks = document.getElementsByTagName('code');
 
@@ -20,25 +23,11 @@ document.addEventListener('DOMContentLoaded', function () {
         });
         clipboard.on('success', function (e) {
             e.clearSelection();
-            // var notification = document.createElement('div');
-            // notification.textContent = 'Copied!';
-            // notification.classList.add('notification');
-            // document.body.appendChild(notification);
-            // setTimeout(function () {
-            // 	notification.style.opacity = '0';
-            // 	setTimeout(function () {
-            // 		document.body.removeChild(notification);
-            // 	}, 1000);
-            // }, 1000);
-            // console.log('已复制到剪贴板:', e.text);
-            // 阻止事件冒泡，避免影响其他逻辑
-            // e.stopPropagation();
             button.textContent = 'Copied!';
 
             setTimeout(() => {
                 button.textContent = 'Copy';
-            }, 3000);
-            e.stopPropagation();
+            }, 1000);
         });
         clipboard.on('error', function (e) {
             console.error('复制失败:', e.action);
